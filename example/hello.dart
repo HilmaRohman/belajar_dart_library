0ff29e5ea71fe0ee9a67ed8c18a51fd7ace87b2a
M. Hilma Minanur Rohman <m.hilmaminanurrohman78@gmail.com>
2022-08-23 04:52:09 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Hilma"));
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,9 @@
+library belajar_dart_library;
+
+// melakukan export file
+// jika tidak terdapat show maka akan mengambil semua nilai
+// show dspat digunakan untuk mengambil class, function dan lain lain
+// export 'src/say_hello.dart';
+export 'src/say_hello.dart' show sayHello;
+
+export 'src/customer.dart' show Category, Product, Customer;
